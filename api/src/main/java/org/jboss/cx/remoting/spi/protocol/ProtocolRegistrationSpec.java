2b8e36ed837a32e16fa3e0bf8b2c75c4f863fa31
David M. Lloyd <david.lloyd@redhat.com>
2007-12-19 05:05:45 +0000

    remove a bunch of useless crap

--- api/src/main/java/org/jboss/cx/remoting/spi/protocol/ProtocolRegistrationSpec.java
+++ api/src/main/java/org/jboss/cx/remoting/spi/protocol/ProtocolRegistrationSpec.java
@@ -11,18 +11,12 @@ public final class ProtocolRegistrationSpec {
     // todo - recheck some of these fields
     private final String scheme;
     private final ProtocolHandlerFactory protocolHandlerFactory;
-    private final CallbackHandler serverCallbackHandler;
-    private final CallbackHandler clientCallbackHandler;
-    private final BufferFactory bufferFactory;
 
-    public static final ProtocolRegistrationSpec DEFAULT = new ProtocolRegistrationSpec(null, null, null, null, BufferFactory.create(8192, false));
+    public static final ProtocolRegistrationSpec DEFAULT = new ProtocolRegistrationSpec(null, null);
 
-    private ProtocolRegistrationSpec(final String scheme, final ProtocolHandlerFactory protocolHandlerFactory, final CallbackHandler serverCallbackHandler, final CallbackHandler clientCallbackHandler, final BufferFactory bufferFactory) {
+    private ProtocolRegistrationSpec(final String scheme, final ProtocolHandlerFactory protocolHandlerFactory) {
         this.scheme = scheme;
         this.protocolHandlerFactory = protocolHandlerFactory;
-        this.serverCallbackHandler = serverCallbackHandler;
-        this.clientCallbackHandler = clientCallbackHandler;
-        this.bufferFactory = bufferFactory;
     }
 
     public String getScheme() {
@@ -33,36 +27,12 @@ public final class ProtocolRegistrationSpec {
         return protocolHandlerFactory;
     }
 
-    public CallbackHandler getServerCallbackHandler() {
-        return serverCallbackHandler;
-    }
-
-    public CallbackHandler getClientCallbackHandler() {
-        return clientCallbackHandler;
-    }
-
-    public BufferFactory getBufferFactory() {
-        return bufferFactory;
-    }
-
     public ProtocolRegistrationSpec setScheme(String scheme) {
-        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory, serverCallbackHandler, clientCallbackHandler, bufferFactory);
+        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory);
     }
 
     public ProtocolRegistrationSpec setProtocolHandlerFactory(ProtocolHandlerFactory protocolHandlerFactory) {
-        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory, serverCallbackHandler, clientCallbackHandler, bufferFactory);
-    }
-
-    public ProtocolRegistrationSpec setServerCallbackHandler(CallbackHandler serverCallbackHandler) {
-        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory, serverCallbackHandler, clientCallbackHandler, bufferFactory);
-    }
-
-    public ProtocolRegistrationSpec setClientCallbackHandler(CallbackHandler clientCallbackHandler) {
-        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory, serverCallbackHandler, clientCallbackHandler, bufferFactory);
-    }
-
-    public ProtocolRegistrationSpec setBufferFactory(BufferFactory bufferFactory) {
-        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory, serverCallbackHandler, clientCallbackHandler, bufferFactory);
+        return new ProtocolRegistrationSpec(scheme, protocolHandlerFactory);
     }
 }
 
