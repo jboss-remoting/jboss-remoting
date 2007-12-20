b98801440f34504903db74b93bd5b7a41d7c7f93
David M. Lloyd <david.lloyd@redhat.com>
2007-12-20 00:01:51 +0000

    Add NULL SASL module

---
+++ sasl-null/src/main/java/org/jboss/cx/remoting/core/security/sasl/NullSaslClientImpl.java
@@ -0,0 +1,42 @@
+package org.jboss.cx.remoting.core.security.sasl;
+
+import javax.security.sasl.SaslClient;
+import javax.security.sasl.SaslException;
+
+/**
+ *
+ */
+public final class NullSaslClientImpl implements SaslClient {
+
+
+    public String getMechanismName() {
+        return "NULL";
+    }
+
+    public boolean hasInitialResponse() {
+        return false;
+    }
+
+    public byte[] evaluateChallenge(byte[] challenge) throws SaslException {
+        return null;
+    }
+
+    public boolean isComplete() {
+        return true;
+    }
+
+    public byte[] unwrap(byte[] incoming, int offset, int len) throws SaslException {
+        throw new IllegalStateException("unwrap()");
+    }
+
+    public byte[] wrap(byte[] outgoing, int offset, int len) throws SaslException {
+        throw new IllegalStateException("wrap()");
+    }
+
+    public Object getNegotiatedProperty(String propName) {
+        return null;
+    }
+
+    public void dispose() throws SaslException {
+    }
+}
