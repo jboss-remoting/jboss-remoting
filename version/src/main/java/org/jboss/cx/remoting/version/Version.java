af95533a192fb459a38bf766731852de6ac0201d
David M. Lloyd <david.lloyd@redhat.com>
2008-02-21 18:11:00 +0000

    Add API javadoc generation

--- version/src/main/java/org/jboss/cx/remoting/version/Version.java
+++ version/src/main/java/org/jboss/cx/remoting/version/Version.java
@@ -7,6 +7,6 @@ public final class Version {
     public static final String VERSION = "3.0.0-M2";
 
     public static void main(String[] args) {
-        System.out.println(VERSION);
+        System.out.print(VERSION);
     }
 }
