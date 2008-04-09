275f4fc9a17e046ea024eff80d481826f5638f7c
David M. Lloyd <david.lloyd@redhat.com>
2008-04-09 03:29:35 +0000

    Message types

--- http/src/main/java/org/jboss/cx/remoting/http/Http.java
+++ http/src/main/java/org/jboss/cx/remoting/http/Http.java
@@ -10,4 +10,28 @@ public final class Http {
 
     private Http() {}
 
+    enum MessageType {
+        SESSION_OPEN,
+        SESSION_CLOSE,
+
+        STREAM_OPEN,
+        STREAM_CLOSE,
+        STREAM_DATA,
+
+        SERVICE_CLOSING,
+
+        SERVICE_CLOSE,
+        SERVICE_CONTEXT_OPEN,
+
+        CONTEXT_CLOSE,
+
+        CONTEXT_CLOSING,
+
+        REQUEST,
+        REQUEST_CANCEL,
+
+        REQUEST_REPLY,
+        REQUEST_EXCEPTION,
+        REQUEST_CANCELLED,
+    }
 }
