1c94da9bbd61c4a7652da8ba83cfde76d640b28c
David M. Lloyd <david.lloyd@redhat.com>
2008-04-16 02:36:54 +0000

    Next version is 3.0.0-M3

--- version/src/main/java/org/jboss/cx/remoting/version/Version.java
+++ version/src/main/java/org/jboss/cx/remoting/version/Version.java
@@ -4,7 +4,7 @@ package org.jboss.cx.remoting.version;
  *
  */
 public final class Version {
-    public static final String VERSION = "3.0.0-M2";
+    public static final String VERSION = "3.0.0-M3";
 
     public static void main(String[] args) {
         System.out.print(VERSION);
