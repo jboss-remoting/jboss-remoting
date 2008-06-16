ecdd89e9d6617b1bc5e5b88fd52ad9aaac4e9b2d
David M. Lloyd <david.lloyd@redhat.com>
2008-06-16 13:16:21 +0000

    Add constructor

--- version/src/main/java/org/jboss/cx/remoting/version/Version.java
+++ version/src/main/java/org/jboss/cx/remoting/version/Version.java
@@ -4,6 +4,10 @@ package org.jboss.cx.remoting.version;
  *
  */
 public final class Version {
+
+    private Version() {
+    }
+
     public static final String VERSION = "3.0.0-M3";
 
     public static void main(String[] args) {
