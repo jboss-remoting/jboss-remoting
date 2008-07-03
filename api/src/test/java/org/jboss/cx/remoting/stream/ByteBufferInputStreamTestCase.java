c133c7351a9a4623ada7daee182f4d1a9a49eeca
David M. Lloyd <david.lloyd@redhat.com>
2008-07-03 23:31:26 +0000

    Add some tests to the api module, plus extra test support

---
+++ api/src/test/java/org/jboss/cx/remoting/stream/ByteBufferInputStreamTestCase.java
@@ -0,0 +1,84 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.cx.remoting.stream;
+
+import junit.framework.TestCase;
+import java.util.Arrays;
+import java.nio.ByteBuffer;
+import org.jboss.cx.remoting.test.support.TestByteBufferAllocator;
+
+/**
+ *
+ */
+public final class ByteBufferInputStreamTestCase extends TestCase {
+    public void testBasic() throws Throwable {
+        final TestByteBufferAllocator allocator = new TestByteBufferAllocator(3);
+        final ByteBufferInputStream stream = new ByteBufferInputStream(Streams.<ByteBuffer>getIteratorObjectSource(Arrays.<ByteBuffer>asList(
+                ByteBuffer.wrap(new byte[] { 5, 100, 30, 12, -60, 25 }),
+                ByteBuffer.wrap(new byte[] { 15 }),
+                ByteBuffer.wrap(new byte[] { }),
+                ByteBuffer.wrap(new byte[] { 100, 0, 0, -128, 127, 0 })).iterator()), allocator);
+        assertEquals(5, stream.read());
+        assertEquals(100, stream.read());
+        assertEquals(30, stream.read());
+        assertEquals(12, stream.read());
+        assertEquals(-60 & 0xff, stream.read());
+        assertEquals(25, stream.read());
+        assertEquals(15, stream.read());
+        assertEquals(100, stream.read());
+        assertEquals(0, stream.read());
+        assertEquals(0, stream.read());
+        assertEquals(-128 & 0xff, stream.read());
+        assertEquals(127, stream.read());
+        assertEquals(0, stream.read());
+        assertEquals(-1, stream.read());
+        assertEquals(-1, stream.read());
+        // I fed it four buffers, so there should be -4
+        allocator.check(-4);
+    }
+
+    public void testArrayRead() throws Throwable {
+        final TestByteBufferAllocator allocator = new TestByteBufferAllocator(3);
+        final ByteBufferInputStream stream = new ByteBufferInputStream(Streams.<ByteBuffer>getIteratorObjectSource(Arrays.<ByteBuffer>asList(
+                ByteBuffer.wrap(new byte[] { 5, 100, 30, 12, -60, 25 }),
+                ByteBuffer.wrap(new byte[] { 15 }),
+                ByteBuffer.wrap(new byte[] { }),
+                ByteBuffer.wrap(new byte[] { 100, 0, 0, -128, 127, 0 })).iterator()), allocator);
+        assertEquals(5, stream.read());
+        assertEquals(100, stream.read());
+        assertEquals(30, stream.read());
+        byte[] bytes = new byte[5];
+        assertEquals(5, stream.read(bytes));
+        assertTrue(Arrays.equals(new byte[] { 12, -60, 25, 15, 100 }, bytes));
+        assertEquals(0, stream.read());
+        bytes = new byte[15];
+        Arrays.fill(bytes, (byte) 7);
+        assertEquals(3, stream.read(bytes, 4, 3));
+        assertTrue(Arrays.equals(new byte[] { 7, 7, 7, 7, 0, -128, 127, 7, 7, 7, 7, 7, 7, 7, 7 }, bytes));
+        assertEquals(0, stream.read());
+        assertEquals(-1, stream.read());
+        assertEquals(-1, stream.read());
+        // I fed it four buffers, so there should be -4
+        allocator.check(-4);
+    }
+}
---
+++ api/src/test/java/org/jboss/cx/remoting/stream/ByteBufferOutputStreamTestCase.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.cx.remoting.stream;
+
+import junit.framework.TestCase;
+import org.jboss.cx.remoting.test.support.TestByteBufferAllocator;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public final class ByteBufferOutputStreamTestCase extends TestCase {
+    public void testBasic() throws Throwable {
+        final TestByteBufferAllocator allocator = new TestByteBufferAllocator(4);
+        final List<ByteBuffer> list = new ArrayList<ByteBuffer>();
+        final ObjectSink<ByteBuffer> sink = Streams.getCollectionObjectSink(list);
+        final ByteBufferOutputStream stream = new ByteBufferOutputStream(sink, allocator);
+        stream.write(new byte[] { 6, 1, 5, 2, 4, 3, 2, 4, 1, 5, 0, 6 });
+        stream.write(new byte[0]);
+        stream.write(new byte[] { 4, 5, 6, 45, -20, 0, 0, 1, 12, 13, 19, 34 }, 3, 7);
+        stream.write(new byte[] { 45, -20, 0, 0, 1, 12, 13 }, 4, 0);
+        stream.write(0);
+        stream.write(10);
+        stream.flush();
+        stream.close();
+        final ByteBufferInputStream inputStream = new ByteBufferInputStream(Streams.getIteratorObjectSource(list.iterator()), allocator);
+        assertEquals(6, inputStream.read());
+        assertEquals(1, inputStream.read());
+        assertEquals(5, inputStream.read());
+        assertEquals(2, inputStream.read());
+        assertEquals(4, inputStream.read());
+        assertEquals(3, inputStream.read());
+        assertEquals(2, inputStream.read());
+        assertEquals(4, inputStream.read());
+        assertEquals(1, inputStream.read());
+        assertEquals(5, inputStream.read());
+        assertEquals(0, inputStream.read());
+        assertEquals(6, inputStream.read());
+        assertEquals(45, inputStream.read());
+        assertEquals(-20 & 0xff, inputStream.read());
+        assertEquals(0, inputStream.read());
+        assertEquals(0, inputStream.read());
+        assertEquals(1, inputStream.read());
+        assertEquals(12, inputStream.read());
+        assertEquals(13, inputStream.read());
+        assertEquals(0, inputStream.read());
+        assertEquals(10, inputStream.read());
+        assertEquals(-1, inputStream.read());
+        inputStream.close();
+        allocator.check(0);
+    }
+}
