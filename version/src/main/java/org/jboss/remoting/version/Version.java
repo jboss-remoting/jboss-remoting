9e2231b083ec45044f462ba9d4ce2c3b7824b4b3
David M. Lloyd <david.lloyd@redhat.com>
2008-11-14 20:08:16 +0000

    Prep beta1!

--- version/src/main/java/org/jboss/remoting/version/Version.java
+++ version/src/main/java/org/jboss/remoting/version/Version.java
@@ -11,7 +11,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "3.0.0-M3";
+    public static final String VERSION = "3.0.0.Beta1";
 
     /**
      * Print the version to {@code System.out}.
