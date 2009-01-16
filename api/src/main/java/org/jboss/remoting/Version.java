0f579c0536f542598dd1fc496b779a2b3e9ca430
David M. Lloyd <david.lloyd@redhat.com>
2009-01-16 16:24:58 +0000

    Next is 3.1.0.CR1

--- api/src/main/java/org/jboss/remoting/Version.java
+++ api/src/main/java/org/jboss/remoting/Version.java
@@ -13,7 +13,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "3.0.0.GA";
+    public static final String VERSION = "3.1.0.CR1";
 
     /**
      * Print the version to {@code System.out}.
