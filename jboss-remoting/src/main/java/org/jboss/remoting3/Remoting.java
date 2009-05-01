16ed7c3e3cfe1ff25e49e93412d4d2b00511bca6
David M. Lloyd <david.lloyd@redhat.com>
2009-05-01 01:01:01 +0000

    Remove unused exception type

--- jboss-remoting/src/main/java/org/jboss/remoting3/Remoting.java
+++ jboss-remoting/src/main/java/org/jboss/remoting3/Remoting.java
@@ -132,53 +132,5 @@ public final class Remoting {
         }
     }
 
-    /**
-     * An exception indicating that there was a problem creating an endpoint.
-     *
-     * @apiviz.exclude
-     */
-    public static final class EndpointException extends RemotingException {
-        private static final long serialVersionUID = -9157350594373125152L;
-
-        /**
-         * Constructs a <tt>EndpointException</tt> with no detail message. The cause is not initialized, and may
-         * subsequently be initialized by a call to {@link #initCause(Throwable) initCause}.
-         */
-        public EndpointException() {
-        }
-
-        /**
-         * Constructs a <tt>EndpointException</tt> with the specified detail message. The cause is not initialized, and
-         * may subsequently be initialized by a call to {@link #initCause(Throwable) initCause}.
-         *
-         * @param msg the detail message
-         */
-        public EndpointException(String msg) {
-            super(msg);
-        }
-
-        /**
-         * Constructs a <tt>EndpointException</tt> with the specified cause. The detail message is set to:
-         * <pre>
-         *  (cause == null ? null : cause.toString())</pre>
-         * (which typically contains the class and detail message of <tt>cause</tt>).
-         *
-         * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method)
-         */
-        public EndpointException(Throwable cause) {
-            super(cause);
-        }
-
-        /**
-         * Constructs a <tt>EndpointException</tt> with the specified detail message and cause.
-         *
-         * @param msg the detail message
-         * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method)
-         */
-        public EndpointException(String msg, Throwable cause) {
-            super(msg, cause);
-        }
-    }
-
     private Remoting() { /* empty */ }
 }
