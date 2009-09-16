77bc68b1e936e2416f92cf198c3ea65071e9fe75
David M. Lloyd <david.lloyd@redhat.com>
2009-09-16 04:25:18 +0000

    javadoc

--- jboss-remoting/src/main/java/org/jboss/remoting3/Options.java
+++ jboss-remoting/src/main/java/org/jboss/remoting3/Options.java
@@ -22,24 +22,57 @@
 
 package org.jboss.remoting3;
 
+/**
+ * Common options for service registration.
+ */
 public final class Options {
 
     private Options() {
     }
 
+    /**
+     * Request that the marshalling layer require the use of one of the listed marshalling protocols, in order of decreasing preference.  If
+     * not specified, use a default value.
+     */
     public static final Option<Sequence<String>> MARSHALLING_PROTOCOLS = Option.sequence("jboss.remoting3.marshalling.protocols", String.class);
 
+    /**
+     * Request that the marshalling layer require the presense of one of the listed user-defined class tables, in order of decreasing preference.  If
+     * not specified, no user class table should be used.
+     */
     public static final Option<Sequence<String>> MARSHALLING_CLASS_TABLES = Option.sequence("jboss.remoting3.marshalling.classTables", String.class);
 
+    /**
+     * Request that the marshalling layer require the presense of one of the listed user-defined object tables, in order of decreasing preference.  If
+     * not specified, no user object table should be used.
+     */
     public static final Option<Sequence<String>> MARSHALLING_OBJECT_TABLES = Option.sequence("jboss.remoting3.marshalling.objectTables", String.class);
 
+    /**
+     * Request that the marshalling layer require the presense of one of the listed class resolvers, in order of decreasing preference.  If
+     * not specified, classes are resolved on the remote side using a default strategy.
+     */
     public static final Option<Sequence<String>> MARSHALLING_CLASS_RESOLVERS = Option.sequence("jboss.remoting3.marshalling.classResolvers", String.class);
 
+    /**
+     * Request that the marshalling layer require the presense of one of the listed object resolvers, in order of decreasing preference.  If
+     * not specified, no object substitution will take place.
+     */
     public static final Option<Sequence<String>> MARSHALLING_OBJECT_RESOLVERS = Option.sequence("jboss.remoting3.marshalling.objectResolvers", String.class);
 
+    /**
+     * Request that the marshalling layer require the presense of one of the listed user-defined externalizer factories, in order of decreasing preference.  If
+     * not specified, no user externalizer factory should be used.
+     */
     public static final Option<Sequence<String>> MARSHALLING_EXTERNALIZER_FACTORIES = Option.sequence("jboss.remoting3.marshalling.externalizerFactories", String.class);
 
+    /**
+     * Specify a metric which is a hint that describes the relative desirability of this service.
+     */
     public static final Option<Integer> METRIC = Option.simple("jboss.remoting3.metric", Integer.class);
 
-    public static final Option<Boolean> EXTERNALLY_VISIBLE = Option.simple("jboss.remoting3.externallyVisible", Boolean.class);
+    /**
+     * Specify that the registered service should or should not be visible remotely.  If not specified, defaults to {@code true}.
+     */
+    public static final Option<Boolean> REMOTELY_VISIBLE = Option.simple("jboss.remoting3.remotelyVisible", Boolean.class);
 }
