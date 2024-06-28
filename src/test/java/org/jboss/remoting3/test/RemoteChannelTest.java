70c78d48f45ea7536a6c86f8ab6fda2dac40085a
Darran Lofthouse <darran.lofthouse@jboss.com>
2024-06-28 18:08:15 +0100

    [REM3-406] Refectoring of the RemoteChannel tests to enable SSL testing.
    This adds support for two variants, pure SSL/TLS and Start TLS.
    
    Keys and certificates are generated using the CAGenerationTool from WildFly Elytron.

--- src/test/java/org/jboss/remoting3/test/RemoteChannelTest.java
+++ src/test/java/org/jboss/remoting3/test/RemoteChannelTest.java
@@ -18,48 +18,22 @@
 
 package org.jboss.remoting3.test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 import static org.xnio.IoUtils.safeClose;
 
-import java.io.Closeable;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.PrivilegedAction;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.SSLContext;
-import javax.security.sasl.SaslServerFactory;
 
-import org.jboss.remoting3.Channel;
 import org.jboss.remoting3.Connection;
-import org.jboss.remoting3.Endpoint;
-import org.jboss.remoting3.OpenListener;
-import org.jboss.remoting3.Registration;
-import org.jboss.remoting3.spi.NetworkServerProvider;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.wildfly.security.auth.client.AuthenticationConfiguration;
-import org.wildfly.security.auth.client.AuthenticationContext;
-import org.wildfly.security.auth.client.MatchRule;
-import org.wildfly.security.auth.realm.SimpleMapBackedSecurityRealm;
-import org.wildfly.security.auth.server.MechanismConfiguration;
-import org.wildfly.security.auth.server.SaslAuthenticationFactory;
-import org.wildfly.security.auth.server.SecurityDomain;
-import org.wildfly.security.password.PasswordFactory;
-import org.wildfly.security.password.spec.ClearPasswordSpec;
-import org.wildfly.security.permission.PermissionVerifier;
-import org.wildfly.security.sasl.SaslMechanismSelector;
+
 import org.wildfly.security.sasl.util.SaslMechanismInformation;
-import org.wildfly.security.sasl.util.ServiceLoaderSaslServerFactory;
-import org.xnio.FutureResult;
 import org.xnio.IoFuture;
 import org.xnio.OptionMap;
 import org.xnio.Options;
@@ -70,83 +44,18 @@ import org.xnio.Options;
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
 public final class RemoteChannelTest extends ChannelTestBase {
-    protected static Endpoint endpoint;
-    private static Closeable streamServer;
-    private Connection connection;
-    private Registration serviceRegistration;
 
+    private static final OptionMap optionMap = OptionMap.create(Options.SSL_ENABLED, Boolean.FALSE);
+    private static final String SASL_MECH = SaslMechanismInformation.Names.SCRAM_SHA_256;
 
     @BeforeClass
     public static void create() throws Exception {
-        endpoint = Endpoint.builder().setEndpointName("test").build();
-        NetworkServerProvider networkServerProvider = endpoint.getConnectionProviderInterface("remote", NetworkServerProvider.class);
-        final SecurityDomain.Builder domainBuilder = SecurityDomain.builder();
-        final SimpleMapBackedSecurityRealm mainRealm = new SimpleMapBackedSecurityRealm();
-        domainBuilder.addRealm("mainRealm", mainRealm).build();
-        domainBuilder.setDefaultRealmName("mainRealm");
-        domainBuilder.setPermissionMapper((permissionMappable, roles) -> PermissionVerifier.ALL);
-        final PasswordFactory passwordFactory = PasswordFactory.getInstance("clear");
-        mainRealm.setPasswordMap("bob", passwordFactory.generatePassword(new ClearPasswordSpec("pass".toCharArray())));
-        final SaslServerFactory saslServerFactory = new ServiceLoaderSaslServerFactory(RemoteChannelTest.class.getClassLoader());
-        final SaslAuthenticationFactory.Builder builder = SaslAuthenticationFactory.builder();
-        builder.setSecurityDomain(domainBuilder.build());
-        builder.setFactory(saslServerFactory);
-        builder.setMechanismConfigurationSelector(mechanismInformation -> SaslMechanismInformation.Names.SCRAM_SHA_256.equals(mechanismInformation.getMechanismName()) ? MechanismConfiguration.EMPTY : null);
-        final SaslAuthenticationFactory saslAuthenticationFactory = builder.build();
-        streamServer = networkServerProvider.createServer(new InetSocketAddress("localhost", 30123), OptionMap.create(Options.SSL_ENABLED, Boolean.FALSE), saslAuthenticationFactory, SSLContext.getDefault());
+        ChannelTestBase.create(optionMap, SASL_MECH, SSLContext.getDefault());
     }
 
     @Before
-    public void testStart() throws IOException, URISyntaxException, InterruptedException {
-        final FutureResult<Channel> passer = new FutureResult<Channel>();
-        serviceRegistration = endpoint.registerService("org.jboss.test", new OpenListener() {
-            public void channelOpened(final Channel channel) {
-                passer.setResult(channel);
-            }
-
-            public void registrationTerminated() {
-            }
-        }, OptionMap.EMPTY);
-        IoFuture<Connection> futureConnection = AuthenticationContext.empty().with(MatchRule.ALL, AuthenticationConfiguration.empty().useName("bob").usePassword("pass").setSaslMechanismSelector(SaslMechanismSelector.NONE.addMechanism("SCRAM-SHA-256"))).run(new PrivilegedAction<IoFuture<Connection>>() {
-            public IoFuture<Connection> run() {
-                try {
-                    return endpoint.connect(new URI("remote://localhost:30123"), OptionMap.EMPTY);
-                } catch (URISyntaxException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        connection = futureConnection.get();
-        assertNull("No SSLSession", connection.getSslSession());
-        IoFuture<Channel> futureChannel = connection.openChannel("org.jboss.test", OptionMap.EMPTY);
-        sendChannel = futureChannel.get();
-        recvChannel = passer.getIoFuture().get();
-        assertNotNull(recvChannel);
-        assertNull("No SSLSession", recvChannel.getConnection().getSslSession());
-//        assertEquals("bob",recvChannel.getConnection().getUserInfo().getUserName());
-    }
-
-    @After
-    public void testFinish() {
-        safeClose(sendChannel);
-        safeClose(recvChannel);
-        safeClose(connection);
-        serviceRegistration.close();
-    }
-
-    @AfterClass
-    public static void destroy() throws IOException, InterruptedException {
-        safeClose(streamServer);
-        safeClose(endpoint);
-        //some environments seem to need a small delay to re-bind the socket
-        // we could alternatively shutdown the xnio worker, this would guarantee
-        // that all tasks were fully completely for closing, but the worker is not
-        // reachable from outside the remoting api
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            //ignore
-        }
+    public void testStart() throws Exception {
+        testStart(SASL_MECH, null, optionMap);
     }
 
     @Test
